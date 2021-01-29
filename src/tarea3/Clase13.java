c170cae0a37de0f66a1f1f00eabe9066ef61d1a9
Gonzalo Roa <gonzalo@multiplica.com>
2021-01-28 19:11:30 -0500

    Clase 14 -ANALISIS DE ETAPAS -TAREA PENDIENTE

---
+++ src/tarea3/Clase13.java
@@ -0,0 +1,23 @@
+package tarea3;
+
+public class Clase13 {
+
+	public static void main(String[] args) {
+		
+		// 1. Se desea conocer los dos primeros numeros  dentro de un arreglo que sumen 15.
+		// [ 1, 3, 5, 5, 7]
+		
+		// Entrada: Arreglo de enteros.
+		/*  PROCESO:
+		 *  FOR - ANIDADO
+		 *  VERIFICAR LA CONDICION PARA EVALUAR SI SE CUMPLE O NO
+		 */ 
+		// Salida: 
+		// Se retorna los dos primeros numeros que sumen 15.
+		
+		// TAREA: INVESTIGAR UN EJERCICIO E IDENTIFICAR LOS PROCESOS PARA RESOLVERLO.	
+		
+
+	}
+
+}
